529e1f4ccf0f5640320cb9762488dd9d3e65f481
Goraniya Karan <karangoraniya@gmail.com>
2023-08-13 21:26:48 +0530

    feat: rainbow adn home page added 🚀

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,80 @@
-import Image from 'next/image'
-
+import Image from 'next/image';
+import { LiaCapsulesSolid } from 'react-icons/lia';
+import { flirt_1, flirt_2, flirt_bg } from '../public/assets/index';
+import Link from 'next/link';
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
+    <main className="bg-gradient-to-r from-black  via-purple-900 to-black">
+      {/* hero section */}
+      <div className="container h-[100vh] w-full flex justify-evenly items-center  bg-cover bg-center bg-no-repeat bg-Custom ">
+        <p className="font-extrabold text-[50px] w-[70%] ">
+          Memories Sealed in Time, Unleashed as NFTs: Explore TimeFlirt&apos;s
+          Unique Fusion
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
+      </div>
+      {/* hero section complete  */}
+      {/* featuers section  start */}
+      <div className="mian_section w-full my-3 flex-col justify-center gap-5 ">
+        {/* card 1 */}
+        <div className="card1 flex md:flex-row flex-col md:gap-[100px] gap-[50px] justify-center items-center md:mb-0 mb-5">
+          {/* img */}
+          <div>
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+              src={flirt_1}
+              alt="Time Flirt"
+              className="h-[300px] w-[300px] rounded-md"
             />
-          </a>
+          </div>
+          {/* content */}
+          <div className="texts w-[400px] text-start flex flex-col md:h-[150px] h-fit ">
+            <p className="text-[20px] font-bold  ">
+              Create your personalized Time Flirt Capsules.
+            </p>
+            <p className="text2">
+              Seal your NFTs in our innovative capsules, designed to withstand
+              the test of time, and open them when it feels right.
+            </p>
+          </div>
+        </div>
+
+        {/* card 2 */}
+        <div className="card1 flex md:flex-row-reverse flex-col-reverse gap-[100px] justify-center items-center">
+          {/* img */}
+          <div>
+            <Image
+              src={flirt_2}
+              alt="Time Flirt"
+              className="h-[300px] w-[300px] rounded-md"
+            />
+          </div>
+          {/* content */}
+          <div className="texts w-[400px] text-start flex flex-col md:h-[150px] h-fit ">
+            <p className="text-[20px] font-bold  ">
+              Unlock endless possibilities in the future.
+            </p>
+            <p className="text2">
+              As time progresses, discover new layers of meaning and value
+              within your capsules.
+            </p>
+          </div>
         </div>
       </div>
+      {/* featuers section  end  */}
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      {/* get started */}
+      <div className="flex flex-col justify-center items-center w-full my-[16%]">
+        <p className="text1 text-[30px] font-bold">Get Started</p>
+        <p className="text2 text-center w-[400px] font-light text-[#A6E1FA]/[0.50]">
+          Ready to flirt with the future? Begin your journey and mint your Time
+          Flirt Capsules today.
+        </p>
+        <Link href="/mint-capsule">
+          <button className="bg-purple-950/[0.70] p-3 rounded-lg flex items-center gap-2 mt-3">
+            Mint Capsules
+            <LiaCapsulesSolid className="text-[25px] font-light " />
+          </button>
+        </Link>
       </div>
     </main>
-  )
+  );
 }
--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ['roadtoweb3.infura-ipfs.io', 'images.weserv.nl'],
+  },
+  reactStrictMode: true,
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false, net: false, tls: false };
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
